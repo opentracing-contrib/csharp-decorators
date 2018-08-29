81878be10b287e5cbb20d4ef26e26441560809bb
Jêrome Rx <jerome+git@rouaix.eu>
2018-08-29 22:46:36 +0200

    better tests

--- test/OpenTracing.Contrib.Decorators.Tests/DecoratorsTest.cs
+++ test/OpenTracing.Contrib.Decorators.Tests/DecoratorsTest.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using OpenTracing.Contrib.Decorators;
 using OpenTracing.Mock;
 using OpenTracing.Noop;
+using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,33 +15,40 @@ namespace OpenTracing.Contrib.Decorators.Tests
     {
         private readonly ITracer _tracer = new MockTracer();
         private readonly ITestOutputHelper _output;
+        private readonly List<string> _outputs = new List<string>();
 
         public DecoratorsTest(ITestOutputHelper output)
         {
             _output = output;
         }
 
+        void WriteLine(string line)
+        {
+            _output.WriteLine(line);
+            _outputs.Add(line);
+        }
+
         class TestOutputTracerDecorator : TracerDecorator
         {
-            private readonly ITestOutputHelper _output;
+            private readonly Action<string> _output;
 
-            public TestOutputTracerDecorator(ITracer tracer, ITestOutputHelper output) : base(tracer)
+            public TestOutputTracerDecorator(ITracer tracer, Action<string> output) : base(tracer)
             {
                 _output = output;
             }
 
             public override ISpanBuilder BuildSpan(string operationName)
             {
-                _output.WriteLine($"Building span named {operationName}");
+                _output($"Building span named {operationName}");
                 return base.BuildSpan(operationName);
             }
         }
 
         class TestOutputSpanBuilderDecorator : SpanBuilderDecorator
         {
-            private readonly ITestOutputHelper _output;
+            private readonly Action<string> _output;
 
-            public TestOutputSpanBuilderDecorator(ISpanBuilder spanBuilder, ITestOutputHelper output) : base(spanBuilder)
+            public TestOutputSpanBuilderDecorator(ISpanBuilder spanBuilder, Action<string> output) : base(spanBuilder)
             {
                 _output = output;
             }
@@ -48,30 +56,30 @@ namespace OpenTracing.Contrib.Decorators.Tests
             public override ISpan Start()
             {
                 var span = base.Start();
-                _output.WriteLine($"Span Started: {span}");
+                _output($"Span Started: {span}");
                 return span;
             }
 
             public override IScope StartActive()
             {
                 var scope = base.StartActive();
-                _output.WriteLine($"Scope Started: {scope.Span}");
+                _output($"Scope Started: {scope.Span}");
                 return scope;
             }
 
             public override IScope StartActive(bool finishSpanOnDispose)
             {
                 var scope = base.StartActive(finishSpanOnDispose);
-                _output.WriteLine($"Scope Started: {scope.Span}");
+                _output($"Scope Started: {scope.Span}");
                 return scope;
             }
         }
 
         class TestOutputScopeDecorator : ScopeDecorator
         {
-            private readonly ITestOutputHelper _output;
+            private readonly Action<string> _output;
 
-            public TestOutputScopeDecorator(IScope scope, ITestOutputHelper output) : base(scope)
+            public TestOutputScopeDecorator(IScope scope, Action<string> output) : base(scope)
             {
                 _output = output;
             }
@@ -79,15 +87,15 @@ namespace OpenTracing.Contrib.Decorators.Tests
             public override void Dispose()
             {
                 base.Dispose();
-                _output.WriteLine($"Scope disposed: {Span}");
+                _output($"Scope disposed: {Span}");
             }
         }
 
         class TestOutputSpanDecorator : SpanDecorator
         {
-            private readonly ITestOutputHelper _output;
+            private readonly Action<string> _output;
 
-            public TestOutputSpanDecorator(ISpan span, ITestOutputHelper output) : base(span)
+            public TestOutputSpanDecorator(ISpan span, Action<string> output) : base(span)
             {
                 _output = output;
             }
@@ -95,7 +103,7 @@ namespace OpenTracing.Contrib.Decorators.Tests
             public override void Finish()
             {
                 base.Finish();
-                _output.WriteLine($"Span Finished: {this}");
+                _output($"Span Finished: {this}");
             }
         }
 
@@ -103,21 +111,21 @@ namespace OpenTracing.Contrib.Decorators.Tests
         public async Task Test()
         {
             var builder = new TracerDecoratorBuilder(_tracer)
-                .WithTracerDecorator(tracer => new TestOutputTracerDecorator(tracer, _output))
-                .WithSpanBuilderDecorator(spanBuilder => new TestOutputSpanBuilderDecorator(spanBuilder, _output))
-                .WithScopeDecorator(scope => new TestOutputScopeDecorator(scope, _output))
-                .WithSpanDecorator(span => new TestOutputSpanDecorator(span, _output))
+                .WithTracerDecorator(tracer => new TestOutputTracerDecorator(tracer, WriteLine))
+                .WithSpanBuilderDecorator(spanBuilder => new TestOutputSpanBuilderDecorator(spanBuilder, WriteLine))
+                .WithScopeDecorator(scope => new TestOutputScopeDecorator(scope, WriteLine))
+                .WithSpanDecorator(span => new TestOutputSpanDecorator(span, WriteLine))
                 ;
 
             var sut = builder.Build();
 
-            using (var scope = sut.BuildSpan("StartActive(fasle)").StartActive(false))
+            using (var scope = sut.BuildSpan("StartActive(false)").StartActive(false))
             {
                 var span = sut.BuildSpan("Start()").Start();
 
                 try
                 {
-                    _output.WriteLine("--> Doing something 1");
+                    WriteLine("--> Doing something 1");
                     await Task.Delay(10);
                 }
                 finally
@@ -128,12 +136,36 @@ namespace OpenTracing.Contrib.Decorators.Tests
                 using (sut.BuildSpan("StartActive()").StartActive())
                 {
                     await Task.Delay(10);
-                    _output.WriteLine("--> Doing something 2");
+                    WriteLine("--> Doing something 2");
                 }
-
 
                 scope.Span.Finish();
             }
+
+            // Asserting only lines with to IDs
+            _outputs.Count.ShouldBe(12);
+            _outputs[0].ShouldBe(@"Building span named StartActive(false)");
+            _outputs[2].ShouldBe(@"Building span named Start()");
+            _outputs[4].ShouldBe(@"--> Doing something 1");
+            _outputs[5].ShouldBe(@"Span Finished: OpenTracing.Contrib.Decorators.Tests.DecoratorsTest+TestOutputSpanDecorator");
+            _outputs[6].ShouldBe(@"Building span named StartActive()");
+            _outputs[8].ShouldBe(@"--> Doing something 2");
+            _outputs[10].ShouldBe(@"Span Finished: OpenTracing.Contrib.Decorators.Tests.DecoratorsTest+TestOutputSpanDecorator");
+
+            /*
+                Building span named StartActive(false)
+                Scope Started: TraceId: 1, SpanId: 2, ParentId: , OperationName: StartActive(false)
+                Building span named Start()
+                Span Started: TraceId: 1, SpanId: 3, ParentId: 2, OperationName: Start()
+                --> Doing something 1
+                Span Finished: OpenTracing.Contrib.Decorators.Tests.DecoratorsTest+TestOutputSpanDecorator
+                Building span named StartActive()
+                Scope Started: TraceId: 1, SpanId: 4, ParentId: 2, OperationName: StartActive()
+                --> Doing something 2
+                Scope disposed: TraceId: 1, SpanId: 4, ParentId: 2, OperationName: StartActive()
+                Span Finished: OpenTracing.Contrib.Decorators.Tests.DecoratorsTest+TestOutputSpanDecorator
+                Scope disposed: TraceId: 1, SpanId: 2, ParentId: , OperationName: StartActive(false)
+            */
         }
     }
 }
